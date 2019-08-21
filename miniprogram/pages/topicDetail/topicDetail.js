5d76126005d64e1ceff83029fcbdecc7a83be213
ma-cherie <240710290@qq.com>
2019-08-22 01:18:57 +0800

    可以评论了

--- miniprogram/pages/topicDetail/topicDetail.js
+++ miniprogram/pages/topicDetail/topicDetail.js
@@ -1,26 +1,28 @@
 // miniprogram/pages/topicDetail/topicDetail.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    item: {},
-
-    imageUrl: ['../../resources/img/avatar.jpg', '../../resources/img/avatar2.jpg', '../../resources/img/avatar3.jpg'],
+    // 问题
+    topic: null,
+    // 输入的评论
     comment: "",
-    
-
-    answerList: null
+    // 回答列表
+    answerList: []
 
   },
 
   // 预览照片
   previewImg: function(){
+    var that = this;
     wx.previewImage({
-      urls: that.data.imageUrl,
+      urls: that.data.topic.imageUrl,
     })
   },
+  // 获取输入的评论内容
   getComment: function(e){
     this.setData({ comment: e.detail.value });
   },
@@ -28,7 +30,53 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // console.log(options.topicIndex);
+    // console.log(topic);
+    const that = this;
+    this.setData({ topic: app.globalData.topicList[options.topicIndex]});
+    this.loadAnswer(this.data.topic.qid);
+  },
 
+  // 加载回答
+  loadAnswer: function (qid) {
+    let that = this;
+    wx.cloud.callFunction({
+      name: 'getAnswer',
+      data: {
+        qid: qid,
+        aLimitNum: 20,
+        aSkipNum:0
+      },
+    })
+    .then(res => {
+      console.log(res);
+      that.setData({
+        answerList: res.result.data
+      })
+    })
+  },
+
+  // 评论
+  sendComment: function(){
+    const that = this;
+    let userInfo = app.globalData.userInfo;
+    wx.cloud.callFunction({
+      name: 'answerquestion',
+      data:{
+        avatarUrl: userInfo.avatarUrl,
+        content: that.data.comment,
+        nickName: userInfo.nickName,
+        uid: wx.getStorageSync("uid"),
+        qid: that.data.topic.qid,
+        hostuid: that.data.topic.uid
+      }
+    }).then(res => {
+      console.log(res);
+      // 清空输入框
+      that.setData({ comment: null });
+      // 刷新回答区域
+      that.loadAnswer(that.data.topic.qid);
+    })
   },
 
   /**
@@ -80,20 +128,5 @@ Page({
 
   },
 
-  // 加载回答
-  loadAnswer: function(qid) {
-    let that = this
-    wx.cloud.callFunction({
-      name: 'getAnswer',
-      data: {
-        qid: qid
-      },
-    })
-    .then(res => {
-      console.log(res)
-      that.setData({
-        answerList: res.result.data
-      })
-    })
-  }
+
 })
